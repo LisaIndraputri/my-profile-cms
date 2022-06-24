320432b6306bb275f6ab2d7a078b431e9adc09ba
Lisa Indraputri <lisaindraputri98@gmail.com>
2022-06-24 16:17:49 +0700

    modal add

--- src/components/modal/ModalAddProjectList.jsx
+++ src/components/modal/ModalAddProjectList.jsx
@@ -1,18 +1,26 @@
 import React, { useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
-import { Button, Modal, Form, Input, Upload, message } from 'antd';
+import { Button, Modal, Form, Input, Upload, message, Select, Radio } from 'antd';
 import { PlusOutlined, LoadingOutlined } from '@ant-design/icons';
 import { toast } from 'react-toastify';
-import { useDispatch } from 'react-redux';
-import { projectCategoryQuery } from '../../features/project/projectSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { projectQuery } from '../../features/projectList/projectListSlice';
+import Editor from 'ckeditor5-custom-build/build/ckeditor'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
 import {
-  API_CREATE_PROJECT_CATEGORY,
-  API_UPDATE_PROJECT_CATEGORY
+  API_CREATE_PROJECT,
+  API_UPDATE_PROJECT
 } from '../../constants/apis'
-import { request } from '../../utils/request'
+import {
+  request
+} from '../../utils/request'
 
-function ModalAddProjectCategory({isEdit = false, data = {}}) {
+function ModalAddProjectList({isEdit = false, data = {}}) {
+  const { TextArea } = Input;
+  const {projectCategory} = useSelector(state => state.project)
   const [form] = Form.useForm();
+  const [value, setValue] = useState(1);
+  const [desc, setDesc] = useState(1);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [loadingImg, setLoadingImg] = useState(false);
@@ -20,12 +28,17 @@ function ModalAddProjectCategory({isEdit = false, data = {}}) {
 
   const dispatch = useDispatch()
 
+  const { Option } = Select;
+
   useEffect(()=> {
     form.setFieldsValue({
       name: data.name || '',
       description: data.description || '',
+      category: data.category,
+      urlLink: data.urlLink
     });
     setImageUrl(data.projectImg || '')
+    setDesc(data.description || '')
   }, [form, data])
 
   const showModal = () => {
@@ -34,20 +47,22 @@ function ModalAddProjectCategory({isEdit = false, data = {}}) {
 
   const handleOk = async () => {
     setIsLoading(true)
-    const {name, description} = form.getFieldsValue()
+    const {name, category, background_image, urlLink} = form.getFieldsValue()
     const params = {
       name,
-      description,
-      projectImg: imageUrl
+      description: desc,
+      projectImg: value === 1 ? imageUrl : background_image,
+      category,
+      urlLink
     }
-    let API_PROJECT = API_CREATE_PROJECT_CATEGORY
+    let API_PROJECT = API_CREATE_PROJECT
     if (isEdit) {
       params.id = data._id
-      API_PROJECT = API_UPDATE_PROJECT_CATEGORY
+      API_PROJECT = API_UPDATE_PROJECT
     }
     const response = await request(API_PROJECT, params, 'POST')
     if (response.success) {
-      await dispatch(projectCategoryQuery())
+      await dispatch(projectQuery())
       setIsModalVisible(false);
     } else {
       toast.error('Failed to Save!')
@@ -93,6 +108,13 @@ function ModalAddProjectCategory({isEdit = false, data = {}}) {
       });
     }
   };
+  const onChange = (value) => {
+  };
+
+  const onChangeRadio = (e) => {
+    setValue(e.target.value);
+  };
+
 
   const uploadButton = (
     <div>
@@ -154,6 +176,12 @@ function ModalAddProjectCategory({isEdit = false, data = {}}) {
             <Input />
           </Form.Item>
           <Form.Item
+            name="urlLink"
+            label="URL Link"
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
             name="description"
             label="Description"
             rules={[
@@ -162,32 +190,75 @@ function ModalAddProjectCategory({isEdit = false, data = {}}) {
               },
             ]}
           >
-            <Input />
+            <CKEditor 
+              editor={ Editor }
+              data={desc}
+              onReady={editor => {
+                console.log('editor is ready to use', editor)
+              }}
+              onChange={(event, editor) => {
+                const data = editor.getData()
+                setDesc(data)
+              }}
+            />
           </Form.Item>
           <Form.Item
-            name="background_image"
-            label="Background Image"
+            name="category"
+            label="Category"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
           >
-            <Upload
-              listType="picture-card"
-              className="avatar-uploader"
-              showUploadList={false}
-              action="https://run.mocky.io/v3/31402637-7225-4f56-8e34-b7907c556bae"
-              beforeUpload={beforeUpload}
-              onChange={handleChange}
+            <Select
+              placeholder="Select a category"
+              onChange={onChange}
             >
-              {imageUrl ? <img src={imageUrl} alt="project-category" style={{ width: '100%' }} /> : uploadButton}
-            </Upload>
+              {
+                projectCategory.map(itm => <Option value={itm.slug} key={itm.slug}>{itm.name}</Option>)
+              }
+            </Select>
           </Form.Item>
+          <Form.Item>
+            <Radio.Group onChange={onChangeRadio} value={value}>
+              <Radio value={1}>Image</Radio>
+              <Radio value={2}>GIF</Radio>
+            </Radio.Group>
+          </Form.Item>
+          {
+            value === 1 ? 
+              <Form.Item
+                name="background_image"
+                label="Background Image"
+              >
+                <Upload
+                  listType="picture-card"
+                  className="avatar-uploader"
+                  showUploadList={false}
+                  action="https://run.mocky.io/v3/31402637-7225-4f56-8e34-b7907c556bae"
+                  beforeUpload={beforeUpload}
+                  onChange={handleChange}
+                >
+                  {imageUrl ? <img src={imageUrl} alt="project-category" style={{ width: '100%' }} /> : uploadButton}
+                </Upload>
+              </Form.Item> : 
+              <Form.Item
+                name="background_image"
+                label="Background Image"
+              >
+                <TextArea rows={4} />
+              </Form.Item>            
+          }
         </Form>
       </Modal>
     </>
   )
 }
 
-ModalAddProjectCategory.propTypes = {
+ModalAddProjectList.propTypes = {
   isEdit: PropTypes.bool,
   data: PropTypes.object,
 }
 
-export default ModalAddProjectCategory
+export default ModalAddProjectList
